5bb6112cd6d0eaa7714a24fd36ab61111ca6ef1e
Bobby Schade <bschade.18@gmail.com>
2019-08-03 08:26:29 -0500

    Updates

---
+++ precache-manifest.15b1777ee457fd8d969b37cf4fda9498.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb15c5f57f73cc8ac9bd329507d26a8a",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "a3fc573f1da5fdcdb56c",
+    "url": "/pomodoro-clock/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/pomodoro-clock/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "a3fc573f1da5fdcdb56c",
+    "url": "/pomodoro-clock/static/js/main.ddf7aca0.chunk.js"
+  },
+  {
+    "revision": "aa817215ef5e490bad59",
+    "url": "/pomodoro-clock/static/js/runtime~main.647dc629.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pomodoro-clock/static/media/logo.5d5d9eef.svg"
+  }
+]);
